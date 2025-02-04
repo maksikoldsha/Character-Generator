b230b4632104fcfcc9611111b7cbf9bc88caa0b7
maksikoldsha <maruskin2007@gmail.com>
2025-02-04 16:09:15 +0700

    Replace Font Awesome icons with custom images and add falling star animation

--- website/js/script.js
+++ website/js/script.js
@@ -10,3 +10,20 @@ document.querySelector('.nav-bar').addEventListener('click', function(e) {
         document.getElementById(tabId).classList.add('active');
     }
 });
+
+// Добавляем звездочки
+function createStar() {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.left = Math.random() * 100 + 'vw';
+    star.style.animationDuration = Math.random() * 2 + 3 + 's'; // Продолжительность падения от 3 до 5 секунд
+    document.body.appendChild(star);
+
+    // Удаляем звездочку после завершения анимации
+    setTimeout(() => {
+        star.remove();
+    }, 5000);
+}
+
+// Создаем звездочки каждые 100 миллисекунд
+setInterval(createStar, 100);
